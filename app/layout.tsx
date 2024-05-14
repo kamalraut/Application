b7220f22645c36e6a655c2875a583a95f86ff7ab
kamalraut <113272187+kamalraut@users.noreply.github.com>
2024-05-14 21:22:17 +0545

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,49 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Providers from "@/components/Providers";
+import DrawerButton from "@/components/DrawerButton";
+import Sidebar from "@/components/Sidebar";
+import Header from "@/components/header/Header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Trendz",
+  description: "A modern E-commerce website",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          <div className="drawer">
+            <DrawerButton />
+            <div className="drawer-content">
+              <div className="min-h-screen flex flex-col">
+                <Header />
+                {children}
+                <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+                  <p>Copyright © 2024 - All right reserved by Trendz</p>
+                </footer>
+              </div>
+            </div>
+            <div className="drawer-side">
+              <label
+                htmlFor="my-drawer"
+                aria-label="close sidebar"
+                className="drawer-overlay"
+              ></label>
+              <Sidebar />
+            </div>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
